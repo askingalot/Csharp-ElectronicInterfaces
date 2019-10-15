492e0367569fe6271f6db7466ac5fec1beb072c7
Andy Collins <askingalot@gmail.com>
2019-10-15 10:29:08 -0500

    classes approach

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ElectricInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
